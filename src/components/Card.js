f14240d79eff3f9271d1b6b1b72acafce0c7ff08
GiselaSolis <gisela.solis.roman@gmail.com>
2020-05-06 13:31:13 -0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className= 'tc bg-white-80 dib br3 pa3 ma2 dim bw2 shadow-5'>
+        <img alt = 'robots' src = {`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2> {name} </h2>
+                <p> {email} </p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
